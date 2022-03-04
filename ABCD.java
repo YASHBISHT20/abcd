efd1051341f376a050845d1ccc7e522cbbfc6232
YASHBISHT20 <96107895+YASHBISHT20@users.noreply.github.com>
2022-03-04 15:42:53 +0530

    Create ABCD.java

---
+++ ABCD.java
@@ -0,0 +1,12 @@
+public class ABCD
+{
+public static void main(String[] args)
+{
+System.out.println("\nYASH BISHT\n");
+main();
+}
+public static void main()
+{
+System.out.println("\nYASH BISHT2\n");
+}
+}
